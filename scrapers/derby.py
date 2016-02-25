6c7032f6f83dc64eaeae468e72c8e645f82c3049
bubbleboy14 <mario.balibrera@gmail.com>
2016-02-25 15:18:39 -0800

    fixed scraper

--- scrapers/derby.py
+++ scrapers/derby.py
@@ -1,19 +1,27 @@
 import os
-from cantools.util import getcsvmod
+from cantools.util import log, getcsvmod
 from model import db, Race
 
-races = []
+races = {}
 
 def scrape():
-  csv = getcsvmod(os.path.join("scrapers", "data", "DerbyData.csv"))
+  dp = os.path.join("scrapers", "data", "DerbyData.csv")
+  log("scraping %s"%(dp,))
+  csv = getcsvmod(dp)
   keys = csv[0]
+  log("keys: %s"%(keys,), 1)
   for row in csv[1:]:
     draw = row[0]
     if draw not in races:
+      log("can't find %s -- querying db"%(draw,), 2)
       race = Race.query(Race.draw_num == draw).get()
       if not race:
+        log("no record of %s -- creating one"%(draw,), 3)
         race = Race(draw_num=draw)
         for index, data in enumerate(row[1:]):
           setattr(race, keys[index], data)
-        races.append(race)
-  db.put_multi(races)
+        races[draw] = race
+  puts = races.values()
+  log("putting %s races"%(len(puts),), 1)
+  db.put_multi(puts)
+  log("finished!", 1)
